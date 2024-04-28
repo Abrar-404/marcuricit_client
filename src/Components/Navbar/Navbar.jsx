6dbe5a681bbdc8e3a9cd9aa48ba6da40ee6b308a
Al Fattaul Islam <139103740+Abrar-404@users.noreply.github.com>
2024-04-28 15:32:41 +0600

    added hamburger btn

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { IoFlaskSharp } from 'react-icons/io5';
+import '../Styles/SliderBtn.css';
 
 const Navbar = () => {
   return (
@@ -6,15 +7,23 @@ const Navbar = () => {
       <div className="navbar bg-base-100 shadow-md">
         {/* sidebar start */}
 
-        <div className="">
+        <div className="mr-10">
           <input id="my-drawer" type="checkbox" className="drawer-toggle" />
           <div className="navbar-start">
             {/* Page content here */}
             <label
               htmlFor="my-drawer"
-              className="btn btn-primary drawer-button"
+              aria-label="close sidebar"
+              className="drawer-overlay hamburger"
             >
-              Open drawer
+              <input type="checkbox" />
+              <svg viewBox="0 0 32 32">
+                <path
+                  class="line line-top-bottom"
+                  d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                ></path>
+                <path class="line" d="M7 16 27 16"></path>
+              </svg>
             </label>
           </div>
           <div className="drawer-side">
